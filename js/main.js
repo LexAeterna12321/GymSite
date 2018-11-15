80bd7160c62b5e7e3eec680ee4f96a945f9d7c66
LexAeterna12321 <lexaeterna12321@gmail.com>
2018-11-15 12:48:26 +0100

    clients section

--- js/main.js
+++ js/main.js
@@ -17,3 +17,22 @@ const weekDaysHandler = () => {
 window.addEventListener("resize", weekDaysHandler);
 
 weekDaysHandler();
+
+//  clients sections dots handler
+
+const dots = document.querySelectorAll(".clients__controls__control");
+const clients = document.querySelectorAll(".clients__client");
+function viewHandler() {
+  dots.forEach(dot =>
+    dot.classList.remove("clients__controls__control--active")
+  );
+  clients.forEach(client => {
+    client.classList.remove("clients__client--active");
+    client.style.left = `100%`;
+  });
+  clients[this.dataset.client].classList.add("clients__client--active");
+  clients[this.dataset.client].style.left = 0;
+  this.classList.add("clients__controls__control--active");
+}
+
+dots.forEach(dot => dot.addEventListener("click", viewHandler));
